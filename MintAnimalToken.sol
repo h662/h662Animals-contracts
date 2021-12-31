bf63b8fa26a94fcf542cacf5cea04243e8dd5e80
h662 <h662hong@gmail.com>
2022-01-01 01:26:36 +0900

    2. Complete MintAnimalToken.sol

--- MintAnimalToken.sol
+++ MintAnimalToken.sol
@@ -7,8 +7,14 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 contract MintAnimalToken is ERC721Enumerable {
     constructor() ERC721("h662Animals", "HAS") {}
 
+    mapping(uint256 => uint256) public animalTypes;
+
     function mintAnimalToken() public {
         uint256 animalTokenId = totalSupply() + 1;
+
+        uint256 animalType = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, animalTokenId))) % 5 + 1;
+
+        animalTypes[animalTokenId] = animalType;
 
         _mint(msg.sender, animalTokenId);
     }
