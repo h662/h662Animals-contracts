b2b9a0844a4468fb334d9e4f7b38970e63bdf620
h662 <h662hong@gmail.com>
2022-01-02 04:30:29 +0900

    3. setForSaleAnimalToken

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+# h662Animals-contracts
---
+++ SaleAnimalToken.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "MintAnimalToken.sol";
+
+contract SaleAnimalToken {
+    MintAnimalToken public mintAnimalTokenAddress;
+
+    constructor (address _mintAnimalTokenAddress) {
+        mintAnimalTokenAddress = MintAnimalToken(_mintAnimalTokenAddress);
+    }
+
+    mapping(uint256 => uint256) public animalTokenPrices;
+
+    uint256[] public onSaleAnimalTokenArray;
+
+    function setForSaleAnimalToken(uint256 _animalTokenId, uint256 _price) public {
+        address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId);
+
+        require(animalTokenOwner == msg.sender, "Caller is not animal token owner.");
+        require(_price > 0, "Price is zero or lower.");
+        require(animalTokenPrices[_animalTokenId] == 0, "This animal token is already on sale.");
+        require(mintAnimalTokenAddress.isApprovedForAll(animalTokenOwner, address(this)), "Animal token owner did not approve token.");
+
+        animalTokenPrices[_animalTokenId] = _price;
+
+        onSaleAnimalTokenArray.push(_animalTokenId);
+    }
+}
